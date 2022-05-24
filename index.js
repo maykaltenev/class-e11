31f0bb43e1435ba2a75b9dbf63bff0c466cff51a
maykaltenev <michaeltenev@gmail.com>
2022-05-24 07:46:48 +0200

    add on lickc

--- index.js
+++ index.js
@@ -34,14 +34,47 @@ e11.add(nievinAhmad);
 e11.add(vincenzoArena);
 // console.log(e11.allClassmates[0].wishYou())
 
+
+
+const getRanHex = (size) => {
+    let result = [];
+    let hexRef = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+    ];
+    for (let n = 0; n < size; n++) {
+        result.push(hexRef[Math.floor(Math.random() * 16)]);
+    }
+    return result.join("");
+};
+
 let container = document.querySelector(".container");
-for (const item of e11.allClassmates) {
-    console.log(item)
-    let message = document.createElement('div');
-    message.classList.add('box');
-    message.textContent = item.wishYou()
-    container.appendChild(message)
+let button = document.querySelector('#myBtn');
+function createDivs() {
+    for (const item of e11.allClassmates) {
+        let bgColor = getRanHex(4);
+        let message = document.createElement('div');
+        message.classList.add('box');
+        message.style.backgroundColor = `#${bgColor}`;
+        message.textContent = item.wishYou()
+        container.appendChild(message)
+    }
 }
+button.addEventListener('click', createDivs)
 
 
 
